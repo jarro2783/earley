ff91412cfa0e3f6619d5bf42c55f697b4898c695
Jarryd Beck <jarro.2783@gmail.com>
2017-08-25 20:35:41 +1000

    fix the timing function

--- timing.sh
+++ timing.sh
@@ -5,7 +5,7 @@ rm -f $STATS
 
 for i in 10 50 100 500 1000 2000; do
   echo -n "$i " | tee -a $STATS
-  ./earley "$(./generate.py $i)" -t >> $STATS
+  ./earley --expression "$(./generate.py $i)" -t >> $STATS
 done
 
 echo
