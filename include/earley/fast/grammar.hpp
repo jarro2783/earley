ad8227afb902d4a75861087728ec4b92bd1d33ca
Jarryd Beck <jarro.2783@gmail.com>
2017-08-30 08:43:37 +1000

    start fast grammar util

---
+++ include/earley/fast/grammar.hpp
@@ -0,0 +1,7 @@
+#include "earley.hpp"
+
+namespace earley::fast::grammar
+{
+  Grammar
+  build_grammar(const ::earley::Grammar& grammar);
+}
---
+++ src/fast/grammar.cpp
@@ -0,0 +1,11 @@
+#include "earley/fast/grammar.hpp"
+
+namespace earley::fast::grammar
+{
+
+Grammar
+build_grammar(const ::earley::Grammar& grammar)
+{
+}
+
+}
