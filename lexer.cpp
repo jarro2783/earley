3cf0ff03b17b8266c9bc5514d7ff3824ee807fa4
Jarryd Beck <jarro.2783@gmail.com>
2017-09-08 18:18:58 +1000

    fix formatting

--- lexer.cpp
+++ lexer.cpp
@@ -12,27 +12,27 @@ enum Tokens
 
 int main()
 {
-    lexertl::rules rules;
-    lexertl::state_machine sm;
+  lexertl::rules rules;
+  lexertl::state_machine sm;
 
-    rules.push("[0-9]+", NUMBER);
-    rules.push("[a-z]+", IDENTIFIER);
-    rules.push("[ \n\t]", sm.skip());
+  rules.push("[0-9]+", NUMBER);
+  rules.push("[a-z]+", IDENTIFIER);
+  rules.push("[ \n\t]", sm.skip());
 
-    lexertl::generator::build(rules, sm);
+  lexertl::generator::build(rules, sm);
 
-    std::string input("abc 012 34");
-    lexertl::smatch results(input.begin(), input.end());
+  std::string input("abc 012 34");
+  lexertl::smatch results(input.begin(), input.end());
 
-    // Read ahead
+  // Read ahead
+  lexertl::lookup(sm, results);
+
+  while (results.id != 0)
+  {
+    std::cout << "Id: " << static_cast<int>(results.id) << ", Token: '" <<
+        results.str () << "'\n";
     lexertl::lookup(sm, results);
+  }
 
-    while (results.id != 0)
-    {
-        std::cout << "Id: " << static_cast<int>(results.id) << ", Token: '" <<
-            results.str () << "'\n";
-        lexertl::lookup(sm, results);
-    }
-
-    return 0;
+  return 0;
 }
