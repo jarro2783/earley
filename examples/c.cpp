36692fdf54fe52887e47ee94a51c6cf0a699fcdb
Jarryd Beck <jarro.2783@gmail.com>
2017-09-22 18:33:39 +1000

    improve c a bit more

--- examples/c.cpp
+++ examples/c.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 
 #include <earley/fast.hpp>
+#include "grammar.hpp"
 #include <lexertl/generator.hpp>
 #include <lexertl/lookup.hpp>
 #include <lexertl/memory_file.hpp>
@@ -14,7 +15,7 @@ namespace
     BREAK,
     CASE,
     CHAR,
-    CONST,
+    CONST, // 260
     CONTINUE,
     DEFAULT,
     DO,
@@ -23,16 +24,45 @@ namespace
     ENUM,
     EXTERN,
     FOR,
+    SHORT,
     TYPEDEF,
-    VOID,
+    VOID, // 270
     WHILE,
 
     // Symbols
+    EQ_OP,
     RIGHT_OP,
+    LEFT_OP,
+    INC_OP,
+    DEC_OP,
+    PTR_OP,
 
     // Others
     IDENTIFIER,
-    I_CONSTANT,
+    CONSTANT,
+    STRING_LITERAL,
+  };
+
+  earley::fast::grammar::TerminalIndices terminals
+  {
+    {"AUTO", Token::AUTO},
+    {"BREAK", Token::BREAK},
+    {"CASE", Token::CASE},
+    {"CHAR", Token::CHAR},
+    {"CONST", Token::CONST},
+    {"CONSTANT", Token::CONSTANT},
+    {"CONTINUE", Token::CONTINUE},
+    {"DEFAULT", Token::DEFAULT},
+    {"IDENTIFIER", Token::IDENTIFIER},
+    {"SHORT", Token::SHORT},
+    {"TYPEDEF", Token::TYPEDEF},
+    {"VOID", Token::VOID},
+    {"WHILE", Token::WHILE},
+
+    {"EQ_OP", Token::EQ_OP},
+    {"RIGHT_OP", Token::RIGHT_OP},
+
+    {"STRING_LITERAL", Token::STRING_LITERAL},
   };
 }
 
@@ -58,6 +88,14 @@ get_tokens(const char* file)
     {"for", Token::FOR},
     {"typedef", Token::TYPEDEF},
     {"void", Token::VOID},
+
+    // symbols
+    {"<<", Token::LEFT_OP},
+    {">>", Token::RIGHT_OP},
+    {"==", Token::EQ_OP},
+    {"\\+\\+", Token::INC_OP},
+    {"--", Token::DEC_OP},
+    {"->", Token::PTR_OP},
   };
 
   std::vector<int> characters{
@@ -68,9 +106,10 @@ get_tokens(const char* file)
     '!',
     '&',
     '~',
-    '.',
     '>',
     '<',
+    '-',
+    '%',
   };
 
   for (auto& [text, token] : symbols) {
@@ -83,13 +122,24 @@ get_tokens(const char* file)
   }
 
   rules.insert_macro("WS", "[ \\t\\v\\n\\f]");
+
+  rules.insert_macro("O", "[0-7]");
+  rules.insert_macro("D", "[0-9]");
   rules.insert_macro("L", "[A-Za-z_]");
   rules.insert_macro("A", "[A-Za-z_0-9]");
   rules.insert_macro("NZ", "[1-9]");
-  rules.insert_macro("D", "[0-9]");
-  rules.insert_macro("HP", "0[xX]");
   rules.insert_macro("H", "[a-fA-F0-9]");
+  rules.insert_macro("HP", "0[xX]");
+  rules.insert_macro("E", "[Ee][+-]?{D}+");
+  rules.insert_macro("FS", "f|F|l|L");
+  rules.insert_macro("IS", "(((u|U)(l|L|ll|LL)?)|((l|L|ll|LL)(u|U)?))");
 
+  rules.insert_macro("CP", "u|U|L");
+  rules.insert_macro("SP", "(u8|u|U|L)");
+  rules.insert_macro("ES", R"*((\\(['"?\\abfnrtv]|[0-7]{1,3}|x[a-fA-F0-9]+)))*");
+
+  rules.push("\\/", '/');
+  rules.push("\\.", '.');
   rules.push("\\(", '(');
   rules.push("\\)", ')');
   rules.push("\\+", '+');
@@ -104,11 +154,22 @@ get_tokens(const char* file)
   rules.push("\\?", '?');
   rules.push("\\|", '|');
 
-  rules.push(">>", Token::RIGHT_OP);
-
   rules.push("{L}{A}*", Token::IDENTIFIER);
-  rules.push("{NZ}{D}*", Token::I_CONSTANT);
-  rules.push("{HP}{H}+", Token::I_CONSTANT);
+
+  // integers
+  rules.push("{NZ}{D}*", Token::CONSTANT);
+  rules.push("{HP}{H}+", Token::CONSTANT);
+  rules.push("0{O}*{IS}?", Token::CONSTANT);
+
+  // character
+  rules.push(R"**({CP}?'([^'\\\n]|{ES})+')**", Token::CONSTANT);
+
+  // floats
+  rules.push("{D}+{E}{FS}?", Token::CONSTANT);
+  rules.push("D*\\.{D}+{E}?{FS}?", Token::CONSTANT);
+
+  // string
+  rules.push(R"**(({SP}?["]([^\\"\n\r]|{ES})*\"{WS}*)+)**", Token::STRING_LITERAL);
 
   rules.push("{WS}", sm.skip());
 
@@ -133,8 +194,21 @@ get_tokens(const char* file)
   {
     if (results.id == results.npos())
     {
-      throw std::string("Unrecognised token starting with ") +
-        std::string(results.first, std::min(results.first + 10, mf.data() + mf.size()));
+      int i = 0;
+      std::cerr << "Last few tokens are: ";
+      for (auto iter = tokens.rbegin(); iter != tokens.rend() && i < 10; ++iter, ++i)
+      {
+        std::cerr << *iter << " ";
+      }
+      std::cerr << std::endl;
+
+      std::ostringstream ss;
+      ss << "Unrecognised token starting with " <<
+        std::string(
+          results.first,
+          std::min(results.first + 30, mf.data() + mf.size()))
+         << " at position " << (results.first - mf.data());
+      throw ss.str();
     }
     else
     {
@@ -147,25 +221,62 @@ get_tokens(const char* file)
   return tokens;
 }
 
+void
+parse_c(const char* file)
+{
+  lexertl::memory_file c_bnf("grammar/c_raw");
+
+  if (c_bnf.data() == 0)
+  {
+    throw "Unable to open grammar/c_raw";
+  }
+
+  std::string c_definition(c_bnf.data(), c_bnf.data() + c_bnf.size());
+
+  auto [grammar, start] = earley::parse_grammar(c_definition);
+  auto tokens = get_tokens(file);
+  earley::fast::TerminalList symbols(tokens.begin(), tokens.end());
+  earley::fast::grammar::Grammar built(start, grammar, terminals);
+
+  earley::fast::Parser parser(built);
+
+  size_t i;
+  try {
+    for (i = 0; i != symbols.size(); ++i)
+    {
+      parser.parse(symbols, i);
+    }
+  } catch(...)
+  {
+    for (size_t j = 0; j <= i; ++j)
+    {
+      std::cout << "-- Set " << j << " --" << std::endl;
+      parser.print_set(j);
+    }
+    throw;
+  }
+}
+
 int main(int argc, char** argv)
 {
   if (argc < 2)
   {
-    std::cout << "Please provide a C file" << std::endl;
+    std::cerr << "Please provide a C file" << std::endl;
+    return 1;
   }
 
   try
   {
-    auto tokens = get_tokens(argv[1]);
+    parse_c(argv[1]);
   }
   catch (const char* c)
   {
-    std::cout << "Error parsing: " << c << std::endl;
+    std::cerr << "Error parsing: " << c << std::endl;
     return 1;
   }
   catch (const std::string& s)
   {
-    std::cout << "Error parsing: " << s << std::endl;
+    std::cerr << "Error parsing: " << s << std::endl;
     return 1;
   }
 
